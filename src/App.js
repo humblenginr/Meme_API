897028b28024cca405a40a6729d0f45f95b829fa
suod-NithishKarthik <nithishkarthik01@gmail.com>
2020-12-19 13:23:46 +0530

    Project done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Button} from "reactstrap"
 
 function App() {
+
+  const [dataFromApi,setDataFromApi] = useState();
+  const [currentMeme,setCurrentMeme] = useState()
+
+  useEffect(() => {
+
+    Axios.get("https://api.imgflip.com/get_memes").then(
+      res => {
+        setDataFromApi(res.data.data.memes) 
+        setCurrentMeme(res.data.data.memes[Math.floor(Math.random() * 10)])
+        
+      }
+    ).catch(
+      err => console.log(err)
+    )
+  },[])
+
+  
+
+  const setRandomMeme = () => {
+    setCurrentMeme(dataFromApi[Math.floor(Math.random() * dataFromApi.length)])
+  }
+   
   return (
     <div className="App">
 
      THIS IS A MEME GENERATOR API
+     <br></br>
+      {currentMeme && <img src={currentMeme.url}></img>}
+      <br></br>
+     <Button color="success" onClick={setRandomMeme}>Next</Button>
     </div>
   );
 }
